d1ec71c46fb64cd9712d97ffc0aedfa407121a1f
evenly-epic-mule <evenly-epic-mule@localhorst>
2018-01-14 02:04:42 +0100

    Enable encounters only for level 30+

--- monocle/worker.py
+++ monocle/worker.py
@@ -800,9 +800,10 @@ class Worker:
 
                 if (normalized not in SIGHTING_CACHE and
                         normalized not in MYSTERY_CACHE):
-                    if (encounter_conf == 'all'
+                    if self.player_level and self.player_level >= 30 and
+                            (encounter_conf == 'all'
                             or (encounter_conf == 'some'
-                            and normalized['pokemon_id'] in conf.ENCOUNTER_IDS)):
+                                and normalized['pokemon_id'] in conf.ENCOUNTER_IDS)):
                         try:
                             await self.encounter(normalized, pokemon.spawn_point_id)
                         except CancelledError:
